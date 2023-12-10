8d0e9c797f6465b865efe770fd019f2b279713a3
reimasu <rei.masuya@gmail.com>
2023-12-10 14:32:32 -0500

    api progress

--- src/filterNav.js
+++ src/filterNav.js
@@ -2,7 +2,9 @@ import {HashRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 import NavBar from "./NavBar/NavBar";
 import Feed from "./Feed/Feed";
+import Explore from "./Explore/Explore";
 import Profile from "./Profile/Profile";
+import './FilterNav.css'
 function FilterNav() {
   return (
     <div className="filterNav">
@@ -10,6 +12,7 @@ function FilterNav() {
       <NavBar /> 
         <Routes>
           <Route path="/Barz/Home" element={<Feed/>}/>
+          <Route path="/Barz/Explore" element={<Explore/>}/>
         </Routes>
     </div>
   );
