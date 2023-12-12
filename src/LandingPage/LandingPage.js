e8ff238404acfae40789c4f542f4615ba78171f7
reimasu <rei.masuya@gmail.com>
2023-12-12 11:56:14 -0500

    landing page styling

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {
     return(
         <div className='d-flex flex-column landing-style'>
             <div className="d-flex flex-row justify-content-center align-items-center landing-title-container">
-                <h1>Barz</h1>
+                <h1 className='px-5'>Barz</h1>
                 <h3>Share your favorite Barz</h3>
             </div>
 
